cb5e49545964207feea6820ec3b0e8d9b6c82b0a
Won il Park <wonilpark@Wons-iMac.local>
2022-02-10 22:03:18 +0900

    add vitejs/plugin-react-refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
+  plugins: [reactRefresh()],
   resolve: {
     alias: [
       {
